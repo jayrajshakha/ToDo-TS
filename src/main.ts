d6c8b4d388501dde1be37ea0fc677f5fddb39330
Jay Rajshakha <129271583+jayrajshakha@users.noreply.github.com>
2023-09-21 15:24:39 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,104 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface todo {
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  title : string,
+  isComplited : boolean,
+  readonly id : string
+
+}
+
+const todos : todo[] = []
+
+const data = document.querySelector('.data') as HTMLDivElement;
+
+const input = document.querySelector('.in') as HTMLInputElement;
+
+const form = document.querySelector('.form') as HTMLFormElement;
+
+
+form.onsubmit =(e:SubmitEvent)=> {
+
+  e.preventDefault()
+
+ const todo : todo={
+    title : input.value,
+    isComplited : false,
+    id : String(Math.random() * Math.random() * 100)
+ }
+
+ todos.push(todo)
+
+ console.log(todos);
+ render(todos);
+
+ input.value = ''
+}
+
+
+const show = (id : string, isComplited : boolean, title:string) => {
+     
+  const div = document.createElement('div') as HTMLDivElement;
+  div.className = 'ac'
+  const inp = document.createElement('input') as HTMLInputElement;
+  inp.setAttribute('type', 'checkbox')
+  const del = document.createElement('button') as HTMLButtonElement
+
+  const para = document.createElement('p') as HTMLParagraphElement
+
+  para.innerText = title
+  inp.checked = isComplited
+  del.id = id
+  del.innerText = 'Delete'
+
+  inp.onchange = () => {
+    para.className = inp.checked ? 'remove' : 'ok'
+  }
+
+  del.onclick =() => {
+
+    rem(id)
+    
+    
+    
+
+
+  }
+
+
+  div.appendChild(inp)
+  div.appendChild(para)
+  div.appendChild(del)
+
+  data.appendChild(div)
+
+   
+ }
+
+ const rem =(id : string) => {
+
+  let idx = todos.findIndex(i => i.id == id)
+  todos.splice(idx, 1)
+  render(todos)
+
+ }
+
+
+
+
+const render = (todos :todo[]) => {
+     data.innerText = ''
+
+  todos.forEach((i) => {
+
+    show (i.id, i.isComplited, i.title)
+
+    }
+    )
+    
+
+
+}
+
+
+ 
